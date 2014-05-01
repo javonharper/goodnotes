f512e510e57e606ceebaf5f409194b277d9a8c3d
Javon Harper <javon.d.harper@gmail.com>
2014-04-30 20:29:20 -0400

    Ahhh... Sinatra

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'http://rubygems.org'
+
+gem 'sinatra', '~> 1.4.5'
+gem 'haml', '~> 4.0.5'
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'haml'
+
+set :haml, :format => :html5
+
+get '/' do
+  haml :index
+end
