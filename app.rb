552e757fa46fa176821779344b033b7fed9d9a71
Javon Harper <javon.d.harper@gmail.com>
2014-05-04 19:30:27 -0400

    Config coffeescript

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'compass'
 require 'sass'
 require 'pry'
 require 'bootstrap-sass'
+require 'coffee-script'
 
 configure do
   Compass.configuration do |config|
@@ -17,6 +18,11 @@ set :sass, Compass.sass_engine_options
 
 get '/' do
   haml :index
+end
+
+get '/application.js' do
+  content_type "text/javascript"
+  coffee :application
 end
 
 get '/stylesheets/application.css' do
---
+++ views/application.coffee
@@ -0,0 +1,2 @@
+$(document).ready ->
+  console.log 'hello world'
