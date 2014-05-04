2f9fa84c840b9c05345b1413fe4f9799d57f2842
Javon Harper <javon.d.harper@gmail.com>
2014-05-04 13:14:58 -0400

    Configure app to use css proprocessor

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'http://rubygems.org'
 
 gem 'sinatra', '~> 1.4.5'
 gem 'haml', '~> 4.0.5'
+gem 'bootstrap-sass', '~> 3.1.1.1'
+gem 'pry', '~> 0.9.12.6'
+gem 'compass', '~> 0.12.6'
--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
 require 'haml'
+require 'compass'
+require 'sass'
+require 'pry'
 
-set :haml, :format => :html5
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views/stylesheets/'
+  end
+end
+
+set :haml, format: :html5
+set :sass, Compass.sass_engine_options
 
 get '/' do
   haml :index
+end
+
+get '/stylesheets/application.css' do
+  sass :application
 end
