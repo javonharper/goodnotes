a92df739a3847031ecff44cafc5629fe6b135fe9
Javon Harper <javon.d.harper@gmail.com>
2014-05-05 20:34:06 -0400

    Use config file for secrets

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/config_file'
 require 'haml'
 require 'compass'
 require 'sass'
-require 'pry'
 require 'bootstrap-sass'
 require 'coffee-script'
+require 'lastfm'
+require 'pry'
+require 'sinatra/reloader' if development?
+require 'cgi'
 
+config_file 'config.yml' 
 configure do
   Compass.configuration do |config|
     config.project_path = File.dirname(__FILE__)
@@ -16,8 +22,33 @@ end
 set :haml, format: :html5
 set :sass, Compass.sass_engine_options
 
+api_key = settings.LASTFM_API_KEY 
+api_secret = settings.LASTFM_SECRET_KEY
+
+lastfm = Lastfm.new(api_key, api_secret)
+NUM_SONGS = 5
+
 get '/' do
   haml :index
+end
+
+get '/search' do
+  query = params['query']
+  results = lastfm.artist.search({artist: query})
+  matches = results['results']['artistmatches']['artist']
+  unless matches.empty?
+    artist = matches.first['name']
+    redirect to("listen/#{CGI::escape(artist)}")
+  end
+end
+
+get '/listen/:artist' do |artist|
+  artist = CGI::unescape(artist)
+  results = lastfm.artist.get_top_tracks({artist: artist})
+  songs = results.first(NUM_SONGS).map {|song| song['name']}
+  haml :listen, locals: {
+    songs: songs
+  }
 end
 
 get '/application.js' do
