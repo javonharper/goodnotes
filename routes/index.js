ffcfbc41decf5b4ec4334b6cd7ed6cbedff3a097
Javon Harper <javon.d.harper@gmail.com>
2018-05-22 13:23:54 -0400

    Move config to config file

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,59 @@
-var express = require('express');
-var router = express.Router();
-var Lastfm = require('lastfmapi');
+// @format
+
+var express = require('express')
+var router = express.Router()
+var Lastfm = require('lastfmapi')
+var config = require('config')
 
 var lastfm = new Lastfm({
-  'api_key' : '5a1964f7a064939dbc6a5fce2570f3f1',
-  'secret' : '6159650243f21ae79d0fd1f2e9f7f886'
-});
+  api_key: config.lastfm.apiKey,
+  secret: config.lastfm.secret,
+})
 
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Listen to the best tracks of any artist - Goodnotes.io'
-  });
-});
+  res.render('index', {
+    title: 'Listen to the best songs of any artist - Goodnotes.io',
+  })
+})
 
 router.get('/search', function(req, res, next) {
-  var query = req.query.query;
+  var query = req.query.query
 
-  lastfm.artist.search({artist: query, limit: 5}, function(err, artist) {
+  lastfm.artist.search({ artist: query, limit: 5 }, function(err, artist) {
     if (err) {
-      next(err);
+      next(err)
     } else {
-      var artist = artist.artistmatches.artist[0].name;
-      res.redirect('/listen/' + encodeString(artist));
+      var artist = artist.artistmatches.artist[0].name
+      res.redirect('/listen/' + encodeString(artist))
     }
-  });
-});
+  })
+})
 
 router.get('/autocomplete/:query', function(req, res, next) {
-  lastfm.artist.search({artist: req.params.query, limit: 3}, function(err, artistResults) {
+  lastfm.artist.search({ artist: req.params.query, limit: 3 }, function(
+    err,
+    artistResults,
+  ) {
     if (err) {
-        console.log("Error: Could not find autocomplete for query: " + req.params.query);
+      console.log(
+        'Error: Could not find autocomplete for query: ' + req.params.query,
+      )
     }
 
-    res.send(artistResults.artistmatches.artist.map(function(artist) { 
-      return {value: artist.name} 
-    }));
-  });
-});
+    res.send(
+      artistResults.artistmatches.artist.map(function(artist) {
+        return { value: artist.name }
+      }),
+    )
+  })
+})
 
 function encodeString(str) {
-  return encodeURIComponent(str).replace(/%20/g, "-");
-};
+  return encodeURIComponent(str).replace(/%20/g, '-')
+}
 
 function decodeString(str) {
-  return decodeURIComponent(str.replace(/-/g, "%20"));
-};
+  return decodeURIComponent(str.replace(/-/g, '%20'))
+}
 
-module.exports = router;
+module.exports = router
--- routes/listen.js
+++ routes/listen.js
@@ -1,133 +1,150 @@
-var express = require('express');
-var router = express.Router();
+// @format
 
-var Lastfm = require('lastfmapi');
-var Youtube = require('youtube-api');
-var Q = require('q');
-var _ = require('underscore');
+var express = require('express')
+var router = express.Router()
+var Lastfm = require('lastfmapi')
+var Youtube = require('youtube-api')
+var Q = require('q')
+var _ = require('underscore')
+var config = require('config')
 
 var lastfm = new Lastfm({
-  'api_key' : '5a1964f7a064939dbc6a5fce2570f3f1',
-  'secret' : '6159650243f21ae79d0fd1f2e9f7f886'
-});
+  api_key: config.lastfm.apiKey,
+  secret: config.lastfm.secret,
+})
 
 Youtube.authenticate({
   type: 'key',
-  key: 'AIzaSyCbw0MmNUhgTmRczQOjX-w0wdxWD_eCxz8'
-});
+  key: config.youtube.key,
+})
 
 router.get('/:artist', function(req, res, next) {
-  var artist = decodeString(req.params.artist);
+  var artist = decodeString(req.params.artist)
 
-  Q.all([
-    getInfo(artist),
-    getTopTracks(artist)
-  ]).then(function(response) {
+  Q.all([getInfo(artist), getTopTracks(artist)]).then(
+    function(response) {
+      var info = response[0]
+      var topTracks = response[1]
 
-    var info = response[0];
-    var topTracks = response[1];
-
-    Q.all(topTracks.map(function(track) {
-      return getTrackVideo(artist, track.name);
-    })).then(function(videoResponse) {
-      res.render('listen', {
-        title: info.name + "'s most popular songs - Goodnotes.io",
-        artist: info.name, 
-        summary: info.summary,
-        emptySummary: info.summary.indexOf("<") === 1,
-        imageUrl: info.image,
-        tags: info.tags,
-        tracks: videoResponse,
-        similarArtists: info.similarArtists
-      });
-    }, function(errs) {
-      return next(errs);
-    });
-  }, function(errs) {
-    return next(errs);
-  });
-});
+      Q.all(
+        topTracks.map(function(track) {
+          return getTrackVideo(artist, track.name)
+        }),
+      ).then(
+        function(videoResponse) {
+          res.render('listen', {
+            title: info.name + "'s most popular songs - Goodnotes.io",
+            artist: info.name,
+            summary: info.summary,
+            emptySummary: info.summary.indexOf('<') === 1,
+            imageUrl: info.image,
+            tags: info.tags,
+            tracks: videoResponse,
+            similarArtists: info.similarArtists,
+          })
+        },
+        function(errs) {
+          return next(errs)
+        },
+      )
+    },
+    function(errs) {
+      return next(errs)
+    },
+  )
+})
 
 var getTopTracks = function(artist) {
-  var deferred = Q.defer();
+  var deferred = Q.defer()
 
-  lastfm.artist.getTopTracks({artist: artist, autocorrect: 1, limit: 5}, function(err, res) {
-    if (err) {
-      deferred.reject();
-    } else {
-      deferred.resolve(res.track.map(function(track) { 
-        return { name: track.name };
-      }));
-    }
-  });
+  lastfm.artist.getTopTracks(
+    { artist: artist, autocorrect: 1, limit: 5 },
+    function(err, res) {
+      if (err) {
+        deferred.reject()
+      } else {
+        deferred.resolve(
+          res.track.map(function(track) {
+            return { name: track.name }
+          }),
+        )
+      }
+    },
+  )
 
-  return deferred.promise;
-};
+  return deferred.promise
+}
 
 var getInfo = function(artist) {
-  var deferred = Q.defer();
+  var deferred = Q.defer()
 
-  lastfm.artist.getInfo({artist: artist, autocorrect: 1}, function(err, info) {
+  lastfm.artist.getInfo({ artist: artist, autocorrect: 1 }, function(
+    err,
+    info,
+  ) {
     if (err) {
-      deferred.reject(err);
+      deferred.reject(err)
     } else {
       deferred.resolve({
         name: info.name,
         summary: info.bio.summary,
         image: info.image[4]['#text'],
-        similarArtists: _.first(_.shuffle(info.similar.artist), 3).map(function(artist) {
+        similarArtists: _.first(_.shuffle(info.similar.artist), 3).map(function(
+          artist,
+        ) {
           return {
             name: artist.name,
             imageUrl: artist.image[4]['#text'],
-            goodnotesUrl: "/listen/" + encodeString(artist.name)
-
+            goodnotesUrl: '/listen/' + encodeString(artist.name),
           }
         }),
-        tags: _.first(info.tags.tag, 3).map(function(tag){ 
-          return tag.name 
-        })
-      });
+        tags: _.first(info.tags.tag, 3).map(function(tag) {
+          return tag.name
+        }),
+      })
     }
-  });
+  })
 
-  return deferred.promise;
-};
+  return deferred.promise
+}
 
 var getTrackVideo = function(artist, track) {
-  var deferred = Q.defer();
+  var deferred = Q.defer()
 
-  Youtube.search.list({
-    q: artist + ' ' + track,
-    part: 'id',
-    maxResults: 1
-  }, function(err, result) {
+  Youtube.search.list(
+    {
+      q: artist + ' ' + track,
+      part: 'id',
+      maxResults: 1,
+    },
+    function(err, result) {
+      if (_.isEmpty(_.first(result.items))) {
+        err = new Error('Cound not find video for ' + artist + ' - ' + track)
+      }
 
-    if (_.isEmpty(_.first(result.items))) {
-      err = new Error("Cound not find video for " + artist + " - " + track);
-    }
+      if (err) {
+        deferred.reject()
+      } else {
+        var videoId = _.first(result.items).id.videoId
 
-    if (err) {
-      deferred.reject();
-    } else {
-      var videoId = _.first(result.items).id.videoId;
+        deferred.resolve({
+          artist: artist,
+          name: track,
+          videoId: videoId,
+        })
+      }
+    },
+  )
 
-      deferred.resolve({
-        artist: artist,
-        name: track,
-        videoId: videoId
-      });
-    }
-  });
-
-  return deferred.promise;
+  return deferred.promise
 }
 
 function encodeString(str) {
-  return encodeURIComponent(str).replace(/%20/g, "-");
-};
+  return encodeURIComponent(str).replace(/%20/g, '-')
+}
 
 function decodeString(str) {
-  return decodeURIComponent(str.replace(/-/g, "%20"));
-};
+  return decodeURIComponent(str.replace(/-/g, '%20'))
+}
 
-module.exports = router;
+module.exports = router
